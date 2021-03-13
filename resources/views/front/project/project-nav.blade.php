66a03226b03f0cd9862fdc973d0b3e578a3d6019
jmit-modern <jmit.agency@gmail.com>
2021-03-13 17:23:55 +0800

    comments count display

--- resources/views/front/project/project-nav.blade.php
+++ resources/views/front/project/project-nav.blade.php
@@ -4,11 +4,13 @@
     <a href="{{route('project.show', $project->id)}}" class="bg-white border-white border-r w-1/4 py-4 px-1 text-center font-medium text-sm" aria-current="page">
       ホーム
     </a>
-    <a href="#" class="border-transparent border-white border-r w-1/4 py-4 px-1 text-center font-medium text-sm bg-gray-200">
-      活動報告
+    <a href="#" class="border-transparent border-white border-r w-1/4 py-4 px-1 text-center font-medium text-sm bg-gray-200 flex items-center justify-center">
+      <span>活動報告</span>
+      <div class="rounded bg-green-500 text-white ml-2 py-0.5 px-2 inline-block">5</div>
     </a>
-    <a href="#" class="border-transparent border-white border-r w-1/4 py-4 px-1 text-center font-medium text-sm bg-gray-200">
-      コメント
+    <a href="#" class="border-transparent border-white border-r w-1/4 py-4 px-1 text-center font-medium text-sm bg-gray-200 flex items-center justify-center">
+      <span>コメント</span>
+      <div class="rounded bg-green-500 text-white ml-2 py-0.5 px-2 inline-block">450</div>
     </a>
   </nav>
 </div>
